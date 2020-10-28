55300e03fe0693ba2d05fbbc5711f6d404571f88
abe <abe@skydio.com>
2020-10-27 18:47:10 -0700

    wip

--- foo_bar.cc
+++ foo_bar.cc
@@ -210,7 +210,7 @@ namespace octomap {
   {
 
 
-// hello. let's make some changes
+// hello. let's make some changes. Hmm. Not so fast
 #ifdef _OPENMP
     omp_set_num_threads(this->keyrays.size());
     #pragma omp parallel for schedule(guided)
